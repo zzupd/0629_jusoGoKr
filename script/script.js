fe94de42b28167a825688a7142a0c16aae0c9bad
zzupd <zzupd@naver.com>
2021-06-30 11:23:05 +0900

    0630_헤더GNB영역&검색영역 작업중

--- script/script.js
+++ script/script.js
@@ -4,10 +4,12 @@ $(function(){
 
     $("li.mainLi").mouseover(function(){
         $("ul.subMenu").show();
+        $("div#gnbBGFrame").show();
     });
 
     $("li.mainLi").mouseout(function(){
         $("ul.subMenu").hide();
+        $("div#gnbBGFrame").hide();
     });
 
 });
