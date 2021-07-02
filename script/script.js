301fac49bb5b5267ad289a350d02c8a8989033a7
zzupd <zzupd@naver.com>
2021-07-02 12:16:04 +0900

    0702_검색영역작업완료

--- script/script.js
+++ script/script.js
@@ -2,6 +2,8 @@
 
 $(function(){
 
+
+
     /* GNB 메뉴 시작 */
     $("li.mainLi").mouseover(function(){
         $("ul.subMenu").show();
@@ -36,7 +38,58 @@ $(function(){
     });     // 2차 기능
     /* 주소 검색 끝 */
 
-});
+
+    /* 검색 내용 제거(closeMark 버튼) 시작 */
+        // 1차기능 => closeMark 클릭
+        // 2차기능 => 텍스트박스(input type="text") 내용 제거
+    $("span#closeMark").click(function(){
+
+        $("input#searchWord").val("");
+
+        $(this).css({"visibility": "hidden"});
+        // 1차기능에서 선택자로 지정된 항목이
+        // 추가기능에서 동일하게 지정할 때
+        // this 선택자를 사용한다.
+
+    });
+    /* 검색 내용 제거(closeMark 버튼) 끝 */
+
+
+    /* 검색창 실행 유효성검사 시작 */
+        // 검색창이 공백일 때 검색 실행 버튼을 누르면
+        // 경고창을 발생하여 내용을 입력하게 만든다.
+        // 이렇게 사용자에게 올바른 행동을 유도하는
+        // 코드를 작성하는 것을 "JS 유효성 검사"라고 한다.
+        //                      Validation 밸리데이션(=유효성)
+
+    $("span#searchMark").click(function(){
+
+        var txt = $("input#searchWord").val();
+        // 변수  => 텍스트박스의 입력값이 임시 저장되어 있음
+
+        if (txt == "") {   // if조건식 또는 if조건문
+            alert("검색어을(를) 입력해주세요!");
+        }
+
+        if (txt != "") {   // 검색어가 공백이 아니라면 데이터 전송
+            $("form").submit();
+        }
+
+
+
+        // 텍스트박스에 포커스 설정하기
+        // $("선택자").포커스설정기능(); => 해당메서드 .focus()
+        $("input#searchWord").focus();
+
+    });
+
+
+    /* 검색창 실행 유효성검사 끝 */
+
+
+
+
+});   // 제이쿼리 템플릿코드
 
 
 
